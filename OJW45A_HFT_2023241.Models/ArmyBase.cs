5792ffa5bfbb11a3b839233068268182535c4d19
Balog Szilárd <balog.szilard@stud.uni-obuda.hu>
2023-10-30 15:23:28 +0100

    Created Models

---
+++ OJW45A_HFT_2023241.Models/ArmyBase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OJW45A_HFT_2023241.Models
+{
+    [Table("ARMY_BASES")]
+    public class ArmyBase
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [Required]
+        public int NumberOfBeds { get; set; }
+
+        public int DateOfBuild { get; set; }
+
+        [NotMapped]
+        public ICollection<Soldier> Soldiers { get; set; }
+
+        public ArmyBase()
+        {
+            Soldiers = new HashSet<Soldier>();                
+        }
+    }
+}
---
+++ OJW45A_HFT_2023241.Models/Equipment.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OJW45A_HFT_2023241.Models
+{
+    [Table("EQUIPMENT")]
+    public class Equipment
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public string Id { get; set; }//Serial number of the equipment
+
+        [Required]
+        [StringLength(50)]
+        public string Type { get; set; }
+
+        [StringLength(200)]
+        public string Description { get; set; }
+
+        public int Weight {  get; set; }
+
+        [ForeignKey(nameof(Soldier))]
+        public int SoldierId { get; set; }
+
+        public Equipment()
+        {
+        }
+    }
+}
