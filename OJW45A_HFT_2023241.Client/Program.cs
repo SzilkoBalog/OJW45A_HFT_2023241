0df56469c365b11e9809d68073d37c0de79174ae
Balog Szilárd <balog.szilard@stud.uni-obuda.hu>
2023-11-19 20:45:42 +0100

    RestService

--- OJW45A_HFT_2023241.Client/Program.cs
+++ OJW45A_HFT_2023241.Client/Program.cs
@@ -7,8 +7,10 @@ namespace OJW45A_HFT_2023241.Client
 {
     public class Program
     {
+        static RestService rest;
         static void Main(string[] args)
         {
+            //rest = new RestService("https://localhost:44379/");
         }
     }
 }
