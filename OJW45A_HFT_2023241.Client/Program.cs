b85e8375ee08a9864e07edf086779ffcfc8ee741
Balog Szilárd <balog.szilard@stud.uni-obuda.hu>
2023-11-19 19:21:32 +0100

    Cleanup

--- OJW45A_HFT_2023241.Client/Program.cs
+++ OJW45A_HFT_2023241.Client/Program.cs
@@ -1,7 +1,4 @@
-﻿using OJW45A_HFT_2023241.Logic;
-using OJW45A_HFT_2023241.Logic.Logics;
-using OJW45A_HFT_2023241.Models;
-using OJW45A_HFT_2023241.Repository;
+﻿using OJW45A_HFT_2023241.Models;
 using System;
 using System.Linq;
 using System.Xml.Linq;
@@ -12,47 +9,6 @@ namespace OJW45A_HFT_2023241.Client
     {
         static void Main(string[] args)
         {
-            var ctx = new ArmyDbContext();
-            var repo = new ArmyBaseRepository(ctx);
-            var test = new ArmyBaseLogic(repo);
-
-            var all = test.ReadAll();
-            var id6 = test.Read(6);
-            test.Delete(6);
-            try
-            {
-                var idrossz = test.Read(6);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
-            try
-            {
-                test.Create(new ArmyBase {Name = "Base Alpha", NumberOfBeds = 100, DateOfBuild = new DateTime(2002, 10, 10) });
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
-            try
-            {
-                test.Create(new ArmyBase { Id = 1, Name = "Base Alpha", DateOfBuild = new DateTime(2002, 10, 10) });
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
-            try
-            {
-                test.Create(new ArmyBase { Id = 1, Name = "Base Alpha", NumberOfBeds = 100});
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
-
-            ;
         }
     }
 }
