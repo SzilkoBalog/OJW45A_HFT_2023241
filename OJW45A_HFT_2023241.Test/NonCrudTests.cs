c1c86d5d94b284ad6bdd9850de59673052d08b45
Balog Szilárd <balog.szilard@stud.uni-obuda.hu>
2023-11-19 18:07:37 +0100

    All Tests written and passed

--- OJW45A_HFT_2023241.Test/NonCrudTests.cs
+++ OJW45A_HFT_2023241.Test/NonCrudTests.cs
@@ -95,27 +95,20 @@ namespace OJW45A_HFT_2023241.Test
                 new Soldier {Id = 2, Age = 35, Weight = 95, Equipment = new List<Equipment>
                 {
                     new Equipment { Type = "TestType1", Weight = 10 },
-                    new Equipment { Type = "TestType1", Weight = 20 },
-                    new Equipment { Type = "TestType1", Weight = 30 }
+                    new Equipment { Type = "TestType2", Weight = 10 },
+                    new Equipment { Type = "TestType3", Weight = 10 }
                 } },
                 new Soldier {Id = 3, Age = 18, Weight = 100, Equipment = new List<Equipment>
                 {
-                    new Equipment { Type = "TestType1", Weight = 10 },
-                    new Equipment { Type = "TestType2", Weight = 20 },
-                    new Equipment { Type = "TestType3", Weight = 30 }
+                    new Equipment { Type = "TestType1", Weight = 1 },
+                    new Equipment { Type = "TestType2", Weight = 1 },
+                    new Equipment { Type = "TestType1", Weight = 0 },
                 } },
                 new Soldier {Id = 4, Age = 42, Weight = 100, Equipment = new List<Equipment>
                 {
-                    new Equipment { Type = "TestType1", Weight = 10 },
-                    new Equipment { Type = "TestType2", Weight = 20 },
-                    new Equipment { Type = "TestType3", Weight = 30 }
+                    new Equipment { Type = "TestType1", Weight = 5 },
                 } },
-                new Soldier {Id = 5, Age = 50, Weight = 65, Equipment = new List<Equipment>
-                {
-                    new Equipment { Type = "TestType1", Weight = 10 },
-                    new Equipment { Type = "TestType1", Weight = 20 },
-                    new Equipment { Type = "TestType1", Weight = 30 }
-                } }
+                new Soldier {Id = 5, Age = 50, Weight = 65 }
             }.AsQueryable();
 
             mockArmyBaseRepository = new Mock<IRepository<ArmyBase>>();
@@ -211,18 +204,18 @@ namespace OJW45A_HFT_2023241.Test
         {
             new KeyValuePair<string, Dictionary<string, int>>("BaseId:1", new Dictionary<string, int>
             {
-                { "TestType1", 6 }, // Expected counts for TestBase1
+                { "TestType1", 6 },
             }),
             new KeyValuePair<string, Dictionary<string, int>>("BaseId:2", new Dictionary<string, int>
             {
                 { "TestType1", 2 },
                 { "TestType2", 2 },
-                { "TestType3", 2 }// Expected counts for TestBase2
+                { "TestType3", 2 }
             }),
             new KeyValuePair<string, Dictionary<string, int>>("BaseId:3", new Dictionary<string, int>
             {
                 { "TestType1", 3 },
-                { "TestType2", 3 },// Expected counts for TestBase3
+                { "TestType2", 3 },
             }),
             new KeyValuePair<string, Dictionary<string, int>>("BaseId:4", new Dictionary<string, int>{}),
             new KeyValuePair<string, Dictionary<string, int>>("BaseId:5", new Dictionary<string, int>{})
@@ -235,15 +228,43 @@ namespace OJW45A_HFT_2023241.Test
         [Test]
         public void GetSoldiersWithEquipmentTypesTest()
         {
+            // Act
+            var result = soldierLogic.GetSoldiersWithEquipmentTypes();
 
-            Assert.Pass();
+            // Assert
+            Assert.IsNotNull(result);
+
+            var expectedResult = new List<KeyValuePair<Soldier, IEnumerable<string>>>
+        {
+            new KeyValuePair<Soldier, IEnumerable<string>>(inputSoldier.First(), new List<string> { "TestType1", "TestType1", "TestType1" }),
+            new KeyValuePair<Soldier, IEnumerable<string>>(inputSoldier.Skip(1).First(), new List<string> { "TestType1", "TestType2", "TestType3" }),
+            new KeyValuePair<Soldier, IEnumerable<string>>(inputSoldier.Skip(2).First(), new List<string> { "TestType1", "TestType2", "TestType1" }),
+            new KeyValuePair<Soldier, IEnumerable<string>>(inputSoldier.Skip(3).First(), new List<string> { "TestType1" }),
+            new KeyValuePair<Soldier, IEnumerable<string>>(inputSoldier.Skip(4).First(), new List<string> {})
+        };
+
+            CollectionAssert.AreEquivalent(expectedResult, result);
         }
 
         [Test]
         public void GetSoldiersWithTotalEquipmentWeightTest()
         {
+            // Act
+            var result = soldierLogic.GetSoldiersWithTotalEquipmentWeight();
 
-            Assert.Pass();
+            // Assert
+            Assert.IsNotNull(result);
+
+            var expectedResult = new List<KeyValuePair<Soldier, int>>
+        {
+            new KeyValuePair<Soldier, int>(inputSoldier.First(), 60),
+            new KeyValuePair<Soldier, int>(inputSoldier.Skip(1).First(), 30),
+            new KeyValuePair<Soldier, int>(inputSoldier.Skip(2).First(), 2),
+            new KeyValuePair<Soldier, int>(inputSoldier.Skip(3).First(), 5),
+            new KeyValuePair<Soldier, int>(inputSoldier.Skip(4).First(), 0)
+        };
+
+            CollectionAssert.AreEquivalent(expectedResult, result);
         }
     }
 }
