6d1f481ed299e2e6ce7312cc74e2376cd20392d3
Balog Szilárd <balog.szilard@stud.uni-obuda.hu>
2023-11-27 15:33:53 +0100

    Exception handling in client crud methods almost done

--- OJW45A_HFT_2023241.Client/Program.cs
+++ OJW45A_HFT_2023241.Client/Program.cs
@@ -2,9 +2,6 @@
 using OJW45A_HFT_2023241.Models;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Numerics;
-using System.Xml.Linq;
 
 namespace OJW45A_HFT_2023241.Client
 {
@@ -15,9 +12,72 @@ namespace OJW45A_HFT_2023241.Client
         {
             if (entity == "ArmyBase")
             {
-                Console.Write("Enter Base Name: ");
-                string name = Console.ReadLine();
-                rest.Post(new ArmyBase() { Name = name }, "armybase");
+                try
+                {
+                    //Name
+                    Console.Write("Enter Base Name: ");
+                    string name = Console.ReadLine();
+                    //Dateofbuild
+                    Console.Write("Enter date of build in DD/MM/YYYY: ");
+                    DateTime dateOfBuild = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy", null);
+                    //Numberofbeds
+                    Console.Write("Enter number of beds: ");
+                    int numberOfBeds = int.Parse(Console.ReadLine());
+                    rest.Post(new ArmyBase() { Name = name, DateOfBuild = dateOfBuild, NumberOfBeds = numberOfBeds }, "armybase");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    Console.ReadLine();
+                }
+            }
+            else if (entity == "Soldier")
+            {
+                try
+                {
+                    //Name
+                    Console.Write("Enter Soldier Name: ");
+                    string name = Console.ReadLine();
+                    //Age
+                    Console.Write("Enter Soldier Age: ");
+                    int age = int.Parse(Console.ReadLine());
+                    //Weight
+                    Console.Write("Enter Soldier Weight: ");
+                    int weight = int.Parse(Console.ReadLine());
+                    //ArmyBaseId
+                    Console.Write("Enter Soldier ArmyBaseId: ");
+                    int armyBaseId = int.Parse(Console.ReadLine());
+                    rest.Post(new Soldier() { Name = name, Age = age, Weight = weight, ArmyBaseId = armyBaseId }, "soldier");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    Console.ReadLine();
+                }
+            }
+            else if (entity == "Equipment")
+            {
+                try
+                {
+                    //Type
+                    Console.Write("Enter Equipment Type: ");
+                    string type = Console.ReadLine();
+                    //Description
+                    Console.Write("Enter Equipment Description: ");
+                    string description = Console.ReadLine();
+                    //Weight
+                    Console.Write("Enter Equipment Weight: ");
+                    int weight = int.Parse(Console.ReadLine());
+                    //SoldierId
+                    Console.Write("Enter Equipment SoldierId: ");
+                    int soldierId = int.Parse(Console.ReadLine());
+                    rest.Post(new Equipment() { Type = type, Description = description, Weight = weight, SoldierId = soldierId }, "equipment");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    Console.ReadLine();
+                }
             }
         }
         static void List(string entity)
@@ -27,7 +87,23 @@ namespace OJW45A_HFT_2023241.Client
                 List<ArmyBase> bases = rest.Get<ArmyBase>("armybase");
                 foreach (var item in bases)
                 {
-                    Console.WriteLine(item.Id + ": " + item.Name);
+                    Console.WriteLine(item.Id + ": " + item.Name + "\tDate of build: " + item.DateOfBuild);
+                }
+            }
+            else if (entity == "Soldier")
+            {
+                List<Soldier> bases = rest.Get<Soldier>("soldier");
+                foreach (var item in bases)
+                {
+                    Console.WriteLine(item.Id + ": " + item.Name + "\tAge: " + item.Age);
+                }
+            }
+            else if (entity == "Equipment")
+            {
+                List<Equipment> bases = rest.Get<Equipment>("equipment");
+                foreach (var item in bases)
+                {
+                    Console.WriteLine(item.Id + ": " + item.Type + "\tWeight: " + item.Weight);
                 }
             }
             Console.ReadLine();
@@ -49,9 +125,47 @@ namespace OJW45A_HFT_2023241.Client
         {
             if (entity == "ArmyBase")
             {
-                Console.Write("Enter ArmyBase id to delete: ");
-                int id = int.Parse(Console.ReadLine());
-                rest.Delete(id, "armybase");
+                try
+                {
+                    Console.Write("Enter ArmyBase id to delete: ");
+                    int id = int.Parse(Console.ReadLine());
+                    rest.Delete(id, "armybase");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    Console.ReadLine();
+                }
+                
+            }
+            else if (entity == "Soldier")
+            {
+                try
+                {
+                    Console.Write("Enter Soldier id to delete: ");
+                    int id = int.Parse(Console.ReadLine());
+                    rest.Delete(id, "soldier");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    Console.ReadLine();
+                }
+                
+            }
+            else if (entity == "Equipment")
+            {
+                try
+                {
+                    Console.Write("Enter Equipment id to delete: ");
+                    int id = int.Parse(Console.ReadLine());
+                    rest.Delete(id, "equipment");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    Console.ReadLine();
+                }
             }
         }
         static void Main(string[] args)
