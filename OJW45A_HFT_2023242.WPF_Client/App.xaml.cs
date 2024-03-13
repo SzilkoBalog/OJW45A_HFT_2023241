c5fd02c41938d355767a8fb2e0cfebeb2451c282
Balog Szilárd <balog.szilard@stud.uni-obuda.hu>
2024-03-13 10:05:59 +0100

    Created RestCollection

---
+++ OJW45A_HFT_2023242.WPF_Client/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace OJW45A_HFT_2023242.WPF_Client
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
