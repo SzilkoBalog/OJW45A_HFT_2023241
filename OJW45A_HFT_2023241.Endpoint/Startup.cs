c4a1618674ba080f58e275afc4204d8722206e79
Balog Szilárd <balog.szilard@stud.uni-obuda.hu>
2024-04-06 19:56:18 +0200

    Initial creation of JS client Setup cors policy setup stylesheet setup script.js

--- OJW45A_HFT_2023241.Endpoint/Startup.cs
+++ OJW45A_HFT_2023241.Endpoint/Startup.cs
@@ -64,6 +64,12 @@ namespace OJW45A_HFT_2023241.Endpoint
                 await context.Response.WriteAsJsonAsync(response);
             }));
 
+            app.UseCors(x => x
+            .AllowCredentials()
+            .AllowAnyMethod()
+            .AllowAnyHeader()
+            .WithOrigins("http://localhost:53027"));
+
             app.UseRouting();
 
             app.UseAuthorization();
